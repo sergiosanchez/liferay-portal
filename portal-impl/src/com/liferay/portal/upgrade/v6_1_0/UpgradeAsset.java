f16e211c0977e194ffaf9ec4de905fb0b7bff79d
Julio Camarero <julio.camarero@liferay.com>
2012-08-24 09:20:55 +0200

    LPS-28548 source formating

--- portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeAsset.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeAsset.java
@@ -179,4 +179,5 @@ public class UpgradeAsset extends UpgradeProcess {
 			"update AssetEntry set classNameId = " + dlFileEntryClassNameId +
 				" where classNameId = " + igImageClassNameId);
 	}
+
 }
